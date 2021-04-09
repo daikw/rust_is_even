e783b6ef080d73c705094d3ab116027d03bfe7f5
daikw <hazuhi@gmail.com>
2021-04-10 08:02:19 +0900

    :+1: boilerplate for is_even api

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(is_even(1), false);
+        assert_eq!(is_even(2), true);
+        assert_eq!(is_even(42), true);
+        assert_eq!(is_even(43), false);
     }
+}
+
+pub fn is_even<T: std::fmt::Debug>(n: T) -> bool {
+    println!("{:?}", n);
+
+    let n = format!("{:?}", n).parse::<i32>().unwrap();
+    n % 2 == 0
 }

fea0f21a134aea8245fdb026097e9fd3cff3f381
daikw <hazuhi@gmail.com>
2021-04-11 16:09:35 +0900

    :+1: impl endpoint caller function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+extern crate anyhow;
+extern crate reqwest;
+extern crate serde_json;
+
+const EP: &str = "https://api.isevenapi.xyz/api/iseven/";
+
+#[allow(dead_code)]
+#[derive(serde::Deserialize)]
+struct IsEvenResult {
+    ad: Option<String>,
+    iseven: Option<bool>,
+    error: Option<String>,
+}
+
+pub async fn is_even<T: std::fmt::Display>(n: T) -> anyhow::Result<bool> {
+    let url = format!("{}{}", EP, n);
+    let res = reqwest::get(url).await?.json::<IsEvenResult>().await?;
+
+    if let Some(iseven) = res.iseven {
+        Ok(iseven)
+    } else if let Some(error) = res.error {
+        Err(anyhow::anyhow!(error))
+    } else {
+        panic!("Unknown response from isEven api. Is it under maintainance?");
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,11 +36,4 @@ mod tests {
         assert_eq!(is_even(42), true);
         assert_eq!(is_even(43), false);
     }
-}
-
-pub fn is_even<T: std::fmt::Debug>(n: T) -> bool {
-    println!("{:?}", n);
-
-    let n = format!("{:?}", n).parse::<i32>().unwrap();
-    n % 2 == 0
 }
